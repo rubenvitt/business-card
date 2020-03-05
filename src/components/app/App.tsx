27e7e465e48e9feba9c2fdfd19b9c62666750a13
Ruben Vitt <ruben.vitt@outlook.de>
2020-03-05 07:01:14 +0100

    feat(frontend): restructuring, add scss and terminal-impl

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Terminal from "../terminal/Terminal.component";
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.test}>
+      <Terminal/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/terminal/Terminal.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Terminal.module.scss';
+
+
+function Terminal() {
+    return (
+        <div className={styles.terminal}>
+            > Hello
+        </div>
+    );
+}
+
+
+export default Terminal;

5ef5cc00545ed4af16fb7bc6f7d0b9faf0458f89
Ruben Vitt <dev@rubeen.dev>
2020-04-02 22:24:09 +0200

    feat(cv): add university & gitlab reference

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Terminal from "../terminal/Terminal.component";
 import styles from './App.module.scss';
+import SourceCodeInfo from '../sourcecodeinfo/SourceCodeInfo.component';
 
 function App() {
   return (
-    <div className={styles.test}>
+    <div className={styles.main}>
       <Terminal/>
+      <SourceCodeInfo />
     </div>
   );
 }
--- src/logic/SystemStructure.ts
+++ src/logic/SystemStructure.ts
@@ -64,7 +64,31 @@ const cv = [
     }
 ];
 
-function cvToString(): string {
+const university = [
+    {
+        name: 'FernUniversität Hagen',
+        topic: 'Compter Science',
+        final: 'Master Of Science',
+        result: 'discontinued',
+        start: '2019', end: '2020'
+    },
+    {
+        name: 'Ostfalia Wolfenbüttel',
+        topic: 'Computer Science',
+        final: 'Bachelor Of Science',
+        result: 'success',
+        start: '2016', end: '2019'
+    },
+    {
+        name: 'Ostfalia Suderburg',
+        topic: 'Applied Computer Science',
+        final: 'Bachelor Of Science',
+        result: 'Switched university',
+        start: '2015', end: '2016'
+    }
+];
+
+function workCvToString(): string {
     let result = '';
     cv.forEach(entry => {
         result += `${entry.role} at ${entry.company} (${entry.start} - ${entry.end})\n`;
@@ -73,11 +97,21 @@ function cvToString(): string {
     return result;
 }
 
+function universityToString(): string {
+    let result = '';
+    university.forEach(entry => {
+        // noinspection LongLine
+        result += `${entry.topic} (${entry.name}) | ${entry.final} - ${entry.result} from ${entry.start} until ${entry.end}\n`;
+    })
+    return result;
+}
+
 const DefaultStructure: Root = new Root(
     [
         new Folder("user", [
             new Folder('cv', [
-                new File('work', cvToString())
+                new File('work', workCvToString()),
+                new File('university', universityToString())
             ])
         ])
     ]
